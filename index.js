29532359dc2967bef737cc0b28445d6921b44189
ololiuhqui <ludovicocaluori@protonmail.com>
2023-03-07 22:47:24 +0100

    1

---
+++ index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+/* DOM ELEMENTS */
+const cardsSelector = document.querySelector('.select-cards');
+const cardsSelectorInput = document.querySelector('.select-cards input');
+const btnPlay = document.querySelector('.select-cards button');
+const inputTooltip = document.querySelector('.input-container .tooltip');
+const btnStartNewGame = document.querySelector('.start-new-game');
+const cardGrid = document.querySelector('.card-grid');
+let cardElms;
+
+/* GAME VARIABLES */
+let score;
+let cards = [];
+
+/* GAME FUNCTIONS */
+
+const init = function () {
+  score = 0;
+  cardsSelector.classList.remove('hidden');
+  cardsSelectorInput.focus();
+};
+init();
+
+const checkInput = function () {
+  // if input is NaN
+  if (!cardsSelectorInput.value) {
+    // add tooltip
+    inputTooltip.classList.remove('hidden');
+    inputTooltip.textContent = 'Insert a number!';
+  }
+  // if input is not a valid number
+  else if (cardsSelectorInput.value > 100) {
+    // add tooltip
+    inputTooltip.classList.remove('hidden');
+    inputTooltip.textContent = 'Insert a valid number! (between 1 and 100)';
+  }
+  // if input is a valid number
+  else if (0 < cardsSelectorInput.value && cardsSelectorInput.value <= 100) {
+    // remove card selector, rendercards and show "start new game" btn
+    cardsSelector.classList.add('hidden');
+    btnStartNewGame.classList.remove('hidden');
+
+    renderCards();
+  }
+};
+
+const renderCards = function () {
+  // render the selected number of cards in DOM
+  for (let i = 0; i < Number(cardsSelectorInput.value); i++) {
+    // give all cards a unique id
+    // give each id an img
+    cardGrid.innerHTML += `
+    <div class="card" id="card-${i}">
+      <div class="content">
+          <div class="card-front">?</div>
+          <div class="card-back">              
+            <img src="/images/img-${i}.jpg" alt="">
+          </div>
+      </div>`;
+  }
+
+  // select all cards in DOM
+  cardElms = document.querySelectorAll('.card');
+
+  for (let i = 0; i < cardElms.length; i++) {
+    // create a "cards" dedicated game variables array for later use with same lenght of cardElms nodelist
+    cards[i] = `card-${i}`;
+
+    // select the card content for the current generated card id
+    const currentCardContent = document.querySelector(`#card-${i} .content`);
+
+    // give any card in CardElms an event handler
+    cardElms[i].addEventListener('click', function () {
+      cardReveal(currentCardContent);
+    });
+  }
+
+  // attach to each card content a background img
+};
+
+const cardReveal = function (cardContent) {
+  cardContent.style.transform = 'rotateY(180deg)';
+};
+
+btnPlay.addEventListener('click', checkInput);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    checkInput();
+  }
+});
