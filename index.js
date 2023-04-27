16ce00a9adff11d62fd4f0745d3c0265569defa2
ololiuhqui <ludovicocaluori@protonmail.com>
2023-04-27 19:27:20 +0200

    first steps in implementing a way to lock the card rotation whenever a card is revealed.

--- index.js
+++ index.js
@@ -13,19 +13,16 @@ let cardElms;
 /*  GAME VARIABLES AND OBJECTS PROTOTYPES*/
 
 let score;
-// create a set to ensure only unique card objects will be in the cards array
 let cards = [];
 
 const cardConstructor = function () {
-  this.cardId, this.coupleValue, this.imgSrc;
+  this.cardId, this.coupleValue, this.imgSrc, this.state, this.domRef;
 };
 
 /*  GAME FUNCTIONS */
 
 // Shuffle array with Fisher-Yates modern shuffle
 const shuffleArray = function (a) {
-  // i has to be at max = the index of the last element in array
-  // and at min = 1
   for (let i = a.length - 1; i > 0; i--) {
     // Create a random number integer j such that 0 ≤ j ≤ i
     const j = Math.floor(Math.random() * (i + 1));
@@ -33,11 +30,6 @@ const shuffleArray = function (a) {
     [a[i], a[j]] = [a[j], a[i]];
   }
   return a;
-};
-
-// Rotate a card
-const cardRotate = function (cardContent, deg) {
-  cardContent.style.transform = `rotateY(${deg})`;
 };
 
 // Hide / Show the menù
@@ -58,7 +50,7 @@ const init = function () {
 };
 init();
 
-// CHECKING INPUT
+// CHECKING PLAYER MENU INPUT
 
 const checkInput = function (button) {
   if (button === 0) {
@@ -81,6 +73,7 @@ const renderCards = function (cardsNumber) {
     // Create card objects from prototype and set the properties
     let currentCard = new cardConstructor();
     currentCard.imgSrc = i;
+    currentCard.state = 'covered';
     // Duplicate the currentCard
     const couple = duplicateCard(currentCard);
     // Push each of the 2 cards in the new couple inside the cards array
@@ -92,6 +85,7 @@ const renderCards = function (cardsNumber) {
   shuffleArray(cards);
 
   for (let i = 0; i < cardsNumber; i++) {
+    // add ID to card object
     cards[i].cardId = i + 1;
 
     // Rendering cards in DOM
@@ -105,6 +99,9 @@ const renderCards = function (cardsNumber) {
             <img src="images/img-${cards[i].imgSrc}.jpg" alt="">
           </div>
       </div>`;
+
+    // add DOM reference to card object
+    cards[i].domRef = document.getElementById(`card-${i}`);
   }
 
   handleRotation();
@@ -119,17 +116,23 @@ const duplicateCard = function (currentCard) {
   for (let i = 0; i < 2; i++) {
     // Define card couple value (0 first and then 1)
     currentCard.coupleValue = i;
-    // Push card into couple
+    // Push a copy of currentCard into couple
     couple.push({ ...currentCard });
   }
 
   return couple;
 };
 
+// FIXME
+
 // PLAYER ROUND
 
 document.addEventListener('click', function (e) {
-  console.log(e.target);
+  let selectedCardElm = e.target.parentNode.parentNode;
+
+  if (selectedCardElm) {
+    console.log(selectedCardElm);
+  }
 });
 
 /*checkSelection(){
@@ -148,7 +151,7 @@ const validateCouple = function (card1, card2) {
   card2.style = yellow;
 }; */
 
-/* HANDLERS */
+/* ANIMATIONS AND HANDLERS */
 
 //  Cards rotation handlers
 
@@ -157,16 +160,17 @@ const handleRotation = function () {
     // select all cards in DOM
     const cardElms = document.querySelectorAll('.card');
     // select the card content for every card HTML id
+    const currentCard = cards[i];
     const currentCardContent = document.querySelector(`#card-${i} .content`);
 
     const addHandlers = function () {
-      // first add handler for reveling
+      // first add handler for revealing
       cardElms[i].addEventListener('click', function () {
-        cardRotate(currentCardContent, '180deg');
+        uncoverCard(currentCard, currentCardContent);
 
         // when the first event is fired, add the second handler
         cardElms[i].addEventListener('click', function () {
-          cardRotate(currentCardContent, '0deg');
+          coverCard(currentCard, currentCardContent);
 
           // when the second event is fired, restart function (re-add first handler)
           addHandlers();
@@ -177,6 +181,36 @@ const handleRotation = function () {
   }
 };
 
+// Remove rotation event handlers
+const removeHandlers = function (cardContent) {
+  cardContent.removeEventListener('click', cardRotate(cardContent, '180deg'));
+  cardContent.removeEventListener('click', cardRotate(cardContent, '0deg'));
+};
+
+// Rotate a card
+const cardRotate = function (cardContent, deg) {
+  cardContent.style.transform = `rotateY(${deg})`;
+};
+
+// Reveal a card
+const uncoverCard = function (card, cardContent) {
+  card.state = 'uncovered';
+  cardRotate(cardContent, '180deg');
+};
+
+// Hide a card
+const coverCard = function (card, cardContent) {
+  card.state = 'covered';
+  cardRotate(cardContent, '0deg');
+};
+
+// Lock a card rotation
+const lockCardRotation = function (cardContent) {
+  removeHandlers(cardContent);
+  cardRotate(cardContent, '180deg');
+};
+
+// Add handlers to menù buttons
 for (let i = 0; i < inputBtns.length; i++) {
   inputBtns[i].addEventListener('click', () => {
     checkInput(i);
